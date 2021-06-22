10347598a7f8ea72acfe4af73f24e1f0761242f2
Artemiy Dudko <thepioh@zoho.com>
2021-06-22 19:57:12 +0300

    build

---
+++ dist/mobx-react-router.js
@@ -0,0 +1,277 @@
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory(require("mobx"));
+	else if(typeof define === 'function' && define.amd)
+		define("MobxReactRouter", ["mobx"], factory);
+	else if(typeof exports === 'object')
+		exports["MobxReactRouter"] = factory(require("mobx"));
+	else
+		root["MobxReactRouter"] = factory(root["Mobx"]);
+})(this, function(__WEBPACK_EXTERNAL_MODULE__0__) {
+return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 1);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports) {
+
+module.exports = __WEBPACK_EXTERNAL_MODULE__0__;
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(2);
+
+
+/***/ }),
+/* 2 */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+
+// EXTERNAL MODULE: external {"root":"Mobx","commonjs2":"mobx","commonjs":"mobx","amd":"mobx"}
+var external_root_Mobx_commonjs2_mobx_commonjs_mobx_amd_mobx_ = __webpack_require__(0);
+
+// CONCATENATED MODULE: ./src/store.js
+var _dec, _dec2, _dec3, _dec4, _dec5, _class, _descriptor, _descriptor2, _descriptor3, _descriptor4, _descriptor5, _descriptor6, _temp;
+
+function _initializerDefineProperty(target, property, descriptor, context) { if (!descriptor) return; Object.defineProperty(target, property, { enumerable: descriptor.enumerable, configurable: descriptor.configurable, writable: descriptor.writable, value: descriptor.initializer ? descriptor.initializer.call(context) : void 0 }); }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _applyDecoratedDescriptor(target, property, decorators, descriptor, context) { var desc = {}; Object.keys(descriptor).forEach(function (key) { desc[key] = descriptor[key]; }); desc.enumerable = !!desc.enumerable; desc.configurable = !!desc.configurable; if ('value' in desc || desc.initializer) { desc.writable = true; } desc = decorators.slice().reverse().reduce(function (desc, decorator) { return decorator(target, property, desc) || desc; }, desc); if (context && desc.initializer !== void 0) { desc.value = desc.initializer ? desc.initializer.call(context) : void 0; desc.initializer = undefined; } if (desc.initializer === void 0) { Object.defineProperty(target, property, desc); desc = null; } return desc; }
+
+function _initializerWarningHelper(descriptor, context) { throw new Error('Decorating class property failed. Please ensure that ' + 'proposal-class-properties is enabled and set to use loose mode. ' + 'To use proposal-class-properties in spec mode with decorators, wait for ' + 'the next major version of decorators in stage 2.'); }
+
+
+var store_RouterStore = (_dec = external_root_Mobx_commonjs2_mobx_commonjs_mobx_amd_mobx_["action"].bound, _dec2 = external_root_Mobx_commonjs2_mobx_commonjs_mobx_amd_mobx_["action"].bound, _dec3 = external_root_Mobx_commonjs2_mobx_commonjs_mobx_amd_mobx_["action"].bound, _dec4 = external_root_Mobx_commonjs2_mobx_commonjs_mobx_amd_mobx_["action"].bound, _dec5 = external_root_Mobx_commonjs2_mobx_commonjs_mobx_amd_mobx_["action"].bound, (_class = (_temp =
+/*#__PURE__*/
+function () {
+  function RouterStore() {
+    _classCallCheck(this, RouterStore);
+
+    _initializerDefineProperty(this, "location", _descriptor, this);
+
+    this.history = null;
+
+    _initializerDefineProperty(this, "push", _descriptor2, this);
+
+    _initializerDefineProperty(this, "replace", _descriptor3, this);
+
+    _initializerDefineProperty(this, "go", _descriptor4, this);
+
+    _initializerDefineProperty(this, "goBack", _descriptor5, this);
+
+    _initializerDefineProperty(this, "goForward", _descriptor6, this);
+
+    Object(external_root_Mobx_commonjs2_mobx_commonjs_mobx_amd_mobx_["makeObservable"])(this);
+  }
+
+  _createClass(RouterStore, [{
+    key: "_updateLocation",
+    value: function _updateLocation(newState) {
+      this.location = newState;
+    }
+    /*
+     * History methods
+     */
+
+  }]);
+
+  return RouterStore;
+}(), _temp), (_descriptor = _applyDecoratedDescriptor(_class.prototype, "location", [external_root_Mobx_commonjs2_mobx_commonjs_mobx_amd_mobx_["observable"]], {
+  configurable: true,
+  enumerable: true,
+  writable: true,
+  initializer: function initializer() {
+    return null;
+  }
+}), _applyDecoratedDescriptor(_class.prototype, "_updateLocation", [external_root_Mobx_commonjs2_mobx_commonjs_mobx_amd_mobx_["action"]], Object.getOwnPropertyDescriptor(_class.prototype, "_updateLocation"), _class.prototype), _descriptor2 = _applyDecoratedDescriptor(_class.prototype, "push", [_dec], {
+  configurable: true,
+  enumerable: true,
+  writable: true,
+  initializer: function initializer() {
+    var _this = this;
+
+    return function (location, state) {
+      _this.history.push(location, state);
+    };
+  }
+}), _descriptor3 = _applyDecoratedDescriptor(_class.prototype, "replace", [_dec2], {
+  configurable: true,
+  enumerable: true,
+  writable: true,
+  initializer: function initializer() {
+    var _this2 = this;
+
+    return function (location, state) {
+      _this2.history.replace(location, state);
+    };
+  }
+}), _descriptor4 = _applyDecoratedDescriptor(_class.prototype, "go", [_dec3], {
+  configurable: true,
+  enumerable: true,
+  writable: true,
+  initializer: function initializer() {
+    var _this3 = this;
+
+    return function (n) {
+      _this3.history.go(n);
+    };
+  }
+}), _descriptor5 = _applyDecoratedDescriptor(_class.prototype, "goBack", [_dec4], {
+  configurable: true,
+  enumerable: true,
+  writable: true,
+  initializer: function initializer() {
+    var _this4 = this;
+
+    return function () {
+      _this4.history.goBack();
+    };
+  }
+}), _descriptor6 = _applyDecoratedDescriptor(_class.prototype, "goForward", [_dec5], {
+  configurable: true,
+  enumerable: true,
+  writable: true,
+  initializer: function initializer() {
+    var _this5 = this;
+
+    return function () {
+      _this5.history.goForward();
+    };
+  }
+})), _class));
+;
+// CONCATENATED MODULE: ./src/sync.js
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; var ownKeys = Object.keys(source); if (typeof Object.getOwnPropertySymbols === 'function') { ownKeys = ownKeys.concat(Object.getOwnPropertySymbols(source).filter(function (sym) { return Object.getOwnPropertyDescriptor(source, sym).enumerable; })); } ownKeys.forEach(function (key) { _defineProperty(target, key, source[key]); }); } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
+
+var sync_syncHistoryWithStore = function syncHistoryWithStore(history, store) {
+  // Initialise store
+  store.history = history; // Handle update from history object
+
+  var handleLocationChange = function handleLocationChange(location) {
+    store._updateLocation(location);
+  };
+
+  var unsubscribeFromHistory = history.listen(handleLocationChange);
+  handleLocationChange(history.location);
+
+  var subscribe = function subscribe(listener) {
+    var onStoreChange = function onStoreChange() {
+      var rawLocation = _objectSpread({}, store.location);
+
+      listener(rawLocation, history.action);
+    }; // Listen for changes to location state in store
+
+
+    var unsubscribeFromStore = Object(external_root_Mobx_commonjs2_mobx_commonjs_mobx_amd_mobx_["observe"])(store, 'location', onStoreChange);
+    listener(store.location, history.action);
+    return unsubscribeFromStore;
+  };
+
+  history.subscribe = subscribe;
+  history.unsubscribe = unsubscribeFromHistory;
+  return history;
+};
+// CONCATENATED MODULE: ./index.js
+/* concated harmony reexport RouterStore */__webpack_require__.d(__webpack_exports__, "RouterStore", function() { return store_RouterStore; });
+/* concated harmony reexport syncHistoryWithStore */__webpack_require__.d(__webpack_exports__, "syncHistoryWithStore", function() { return sync_syncHistoryWithStore; });
+
+
+
+/***/ })
+/******/ ]);
+});
